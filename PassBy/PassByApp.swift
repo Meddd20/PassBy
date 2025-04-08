9a1d7ebb627a196edf0a0c2d05f5375923479263
Medhiko Biraja <mbirajabiraja@gmail.com>
2025-04-08 13:15:30 +0800

    Initial Commit

---
+++ PassBy/PassByApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassByApp.swift
+//  PassBy
+//
+//  Created by Medhiko Biraja on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PassByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PassByTests/PassByTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassByTests.swift
+//  PassByTests
+//
+//  Created by Medhiko Biraja on 08/04/25.
+//
+
+import Testing
+@testable import PassBy
+
+struct PassByTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
